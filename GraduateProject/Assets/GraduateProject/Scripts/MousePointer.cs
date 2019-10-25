8b5345439be1a6660c2c92cfc7dfdb91cb573874
keyjjang5 <keyjjang5@naver.com>
2019-10-25 19:06:03 +0900

    카메라 움직임 구현
    Enter The Gungeon의 마우스를 따라서 카메라가 움직이되 캐릭터가 시야에서 벗어나지는 않는 카메라를 구현했다. Cinemachine을 사용했다

---
+++ GraduateProject/Assets/GraduateProject/Scripts/MousePointer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MousePointer : MonoBehaviour
+{
+    private GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("unitychan");    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lookAtMouse();
+    }
+
+    void lookAtMouse()
+    {
+        Vector3 lookTarget = new Vector3(0, 0, 0);
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            lookTarget = hit.point;
+            lookTarget.y = transform.position.y;
+        }
+        if (Vector3.Distance(player.transform.position, lookTarget) < 5.0f)
+            transform.position = lookTarget;
+        else
+        {
+            transform.position = player.transform.position + Vector3.Normalize(lookTarget - player.transform.position) * 5;
+        }
+    }
+}
